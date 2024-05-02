1573dae61cc4382a1b44654937298c24eff4b926
FelipeTorquato <felipectorquato1@gmail.com>
2024-05-02 11:12:04 -0300

    Adicionando routing e componente Home

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello User, Welcome on Home Page</h1>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Home'
+}
+</script>
