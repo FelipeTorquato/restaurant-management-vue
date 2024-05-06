0ea07a0e1e3a2d1ca5825af94d7ca248860e0330
FelipeTorquato <felipectorquato1@gmail.com>
2024-05-06 09:33:48 -0300

    Adicionando Header e Logout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+  <Header/>
   <h1>Hello User, Welcome on Home Page</h1>
 </template>
 
 <script>
+import Header from './Header.vue'
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
+  components: {
+    Header
+  },
   mounted() {
     let user = localStorage.getItem("user-info");
     if (!user) {
